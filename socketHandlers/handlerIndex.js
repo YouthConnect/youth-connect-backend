d5f0689f080977553511749dcf12b8ca01b2eec3
Ethan Storm <stormethan10@gmail.com>
2023-07-24 17:06:13 -0700

    Update handlerIndex.js

--- socketHandlers/handlerIndex.js
+++ socketHandlers/handlerIndex.js
@@ -10,80 +10,65 @@ const { imageModule } = require("../server/models");
 // HANDLE MESSAGES ON THE SERVER SIDE
 const message = async (payload, socket, isImage, recentMessages) => {
   if (payload.text !== "") {
-    try {
-      let newMessage = {};
+    let newMessage = {};
 
-      const currentRoomMessages = `${payload.room}RecentMessages`;
-      //if the message (room specific) queue doesn't exist create it
-      if (!recentMessages[currentRoomMessages]) {
-        recentMessages[currentRoomMessages] = [];
-      }
+    const currentRoomMessages = `${payload.room}RecentMessages`;
+    //if the message (room specific) queue doesn't exist create it
+    if (!recentMessages[currentRoomMessages]) {
+      recentMessages[currentRoomMessages] = [];
+    }
+
+    if (isImage) {
+    } else {
+      let cleanWords1 = filter1.clean(payload.text);
+      let cleanWords2 = filter2.clean(cleanWords1);
+
+      newMessage = {
+        text: cleanWords2,
+        room: payload.room,
+        username: payload.username,
+      };
+    }
+
+    //* Then send it to the other clients */
+
+    // create and manage a list of most recent messages //? so they can be displayed in the terminal
+
+    if (recentMessages[currentRoomMessages].length > 30) {
+      // remove last message, do nothing with it
+      let lastMessage = recentMessages[currentRoomMessages].shift();
+      console.log("removed message from last 30:", lastMessage);
+    }
+
+    if (!isImage) {
+      socket.to(payload.room).emit("NEW MESSAGE", newMessage);
+    } else {
+      //* Then send it to database */
 
       if (isImage) {
-      } else {
-        let cleanWords1 = filter1.clean(payload.text);
-        let cleanWords2 = filter2.clean(cleanWords1);
-
-        newMessage = {
-          text: cleanWords2,
-          room: payload.room,
+        const imageRecord = await imageModule.create(payload);
+        // send image back to everyone
+        console.log("Image Record created!", imageRecord);
+        let image = imageRecord.toJSON();
+        console.log(image);
+        let newMessage = {
+          text: "Image ",
+          image: image,
           username: payload.username,
+          room: payload.room,
         };
-      }
-
-      //* Then send it to the other clients */
-
-
-      // create and manage a list of most recent messages //? so they can be displayed in the terminal
-
-      if (recentMessages[currentRoomMessages].length > 30) {
-        // remove last message, do nothing with it
-        let lastMessage = recentMessages[currentRoomMessages].shift();
-        console.log("removed message from last 30:", lastMessage);
-      }
-
-      if (!isImage) {
-        socket.to(payload.room).emit("NEW MESSAGE", newMessage);
+        socket.emit("NEW MESSAGE", newMessage);
       } else {
-
-      //* Then send it to database */
-      try {
-        if (isImage) {
-          try {
-            const imageRecord = await imageModule.create(payload);
-            // send image back to everyone
-            console.log("Image Record created!", imageRecord);
-            let image = imageRecord.toJSON()
-            console.log(image)
-            let newMessage = {
-              text: 'Image ',
-              image: image,
-              username: payload.username,
-              room: payload.room
-            }
-            socket.emit("NEW MESSAGE", newMessage);
-          } catch (error) {
-            console.log("Error creating new image", error);
-          }
-        } else {
-          try {
-            let createdMessage = await axios.post(
-              `http://localhost:3001/api/v1/messages`,
-              newMessage
-            );
-            console.log(
-              "This is the created message:",
-              createdMessage.data.text
-            );
-          } catch (error) {
-            console.log("ERROR CREATING MESSAGE", error);
-          }
+        try {
+          let createdMessage = await axios.post(
+            `http://localhost:3001/api/v1/messages`,
+            newMessage
+          );
+          console.log("This is the created message:", createdMessage.data.text);
+        } catch (error) {
+          console.log("ERROR CREATING MESSAGE", error);
         }
-      } catch (error) {
-        console.log("ERROR ADDING MESSAGE OR IMAGE TO DATABASE:", error);
       }
-    } catch (error) {
-      console.log("Error creating collection object:", error);
     }
   }
 };
